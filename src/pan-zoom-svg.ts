081746f8ef16977fbe0abd1bc6e37cf732c232b5
fred <jan.winstroth@outlook.com>
2020-10-12 23:32:15 +0200

    Refactoring

--- src/pan-zoom-svg.ts
+++ src/pan-zoom-svg.ts
@@ -1,81 +1,76 @@
-import {LitElement, html, customElement, property, svg, query} from 'lit-element';
-import {styleMap} from 'lit-html/directives/style-map.js';
+import {
+  LitElement,
+  html,
+  customElement,
+  property,
+  svg,
+  query,
+} from 'lit-element';
 import {Square} from './interfaces/square-interface';
 import {Point} from './interfaces/point_interface';
-import { invertYAxis } from './utils';
+import {invertYAxis} from './utils';
 
 function squareTemplate(
-  id: number,
   square: Square,
-  handleSquareMouseEnter: (e: MouseEvent) => void,
-  handleSquareMouseLeave: (e: MouseEvent) => void,
-  handleSquareClick: (e: MouseEvent) => void,
-  handleSquareMouseDown: (e: MouseEvent) => void
+  onMouseEnter: (e: MouseEvent) => void,
+  onMouseLeave: (e: MouseEvent) => void,
+  onClick: (e: MouseEvent) => void,
+  onMouseDown: (e: MouseEvent) => void
 ) {
   // Default style
   let strokeColor = 'black';
   let strokeWidth = '0';
   let cursorStyle = 'grab';
-  // Highlighted only
   if (square.isHighligted && !square.isSelected) {
+    // Highlighted only
     strokeColor = 'rgb(85,160,185)';
     strokeWidth = '2';
-  }
-  // Selected only
-  if (square.isSelected && !square.isHighligted) {
+  } else if (square.isSelected && !square.isHighligted) {
+    // Selected only
     strokeColor = 'rgb(175,35,95)';
     strokeWidth = '2';
-  }
- // Highlighted and selected   
- if (square.isSelected && square.isHighligted) {
+  } else if (square.isSelected && square.isHighligted) {
+    // Highlighted and selected
     strokeColor = 'rgb(175,35,95)';
     strokeWidth = '2';
     cursorStyle = 'move';
   }
 
-  const styles = {
-    cursor: cursorStyle,
-    stroke: strokeColor,
-    strokeWidth: strokeWidth,
-  };
-  let location = {
-    x: square.coordinate.x - square.sideLength / 2.0,
-    y: square.coordinate.y + square.sideLength / 2.0
-  };
-  location = invertYAxis(location);
+  const center = invertYAxis(square.coordinate);
 
   return svg`
     <rect 
-      id=${'square' + id}
-      x=${location.x} 
-      y=${location.y} 
+      x=${-square.sideLength / 2} 
+      y=${-square.sideLength / 2} 
       width=${square.sideLength} 
       height=${square.sideLength} 
       rx=${square.sideLength / 10}
       ry=${square.sideLength / 10}
-      transform='rotate (${square.rotation} ${square.coordinate.x} ${square.coordinate.y})'
-      @mouseenter=${handleSquareMouseEnter}
-      @mouseleave=${handleSquareMouseLeave}
-      @click=${handleSquareClick}
-      @mousedown=${handleSquareMouseDown}
-      style=${styleMap(styles)}
+      stroke=${strokeColor}
+      stroke-width=${strokeWidth}
+      cursor=${cursorStyle}
+      transform='translate(${center.x} ${center.y}) rotate(${square.rotation})'
+      @mouseenter=${onMouseEnter}
+      @mouseleave=${onMouseLeave}
+      @click=${onClick}
+      @mousedown=${onMouseDown}
     ></rect>
   `;
 }
 
 function originTemplate(origin: Point, size: number, strokeWidth: number) {
-  let markerSize = 3 * strokeWidth;
-  let xArrow = { x: origin.x + size, y: origin.y };
-  let yArrow = { x: origin.x, y: origin.y + size };
-  let xLabel = { x: xArrow.x - 4*markerSize , y: xArrow.y - 3*markerSize };
-  let yLabel = { x: yArrow.x - 3*markerSize , y: yArrow.y - 4*markerSize };
+  const markerSize = 3 * strokeWidth;
+  let xArrow = {x: origin.x + size, y: origin.y};
+  let yArrow = {x: origin.x, y: origin.y + size};
+  let xLabel = {x: xArrow.x - 4 * markerSize, y: xArrow.y - 3 * markerSize};
+  let yLabel = {x: yArrow.x - 3 * markerSize, y: yArrow.y - 4 * markerSize};
   // Invert y-axis
   xArrow = invertYAxis(xArrow);
   yArrow = invertYAxis(yArrow);
   xLabel = invertYAxis(xLabel);
   yLabel = invertYAxis(yLabel);
   const invOrigin = invertYAxis(origin);
-    
+
   return svg`
     <marker 
       id="arrow" 
@@ -106,10 +101,10 @@ function originTemplate(origin: Point, size: number, strokeWidth: number) {
 export class PanZoomSvg extends LitElement {
   @property({type: Array})
   squares: Square[] = [];
-  
-  @property({type: Number})  
+
+  @property({type: Number})
   viewportWidth = 0;
-  
+
   @property({type: Number})
   viewportHeight = 0;
 
@@ -119,19 +114,19 @@ export class PanZoomSvg extends LitElement {
   @query('svg')
   svg!: SVGAElement;
 
-  draggedSqaure: Square | undefined = undefined;
-  dragStartPointerPosition = {x: 0, y: 0};
+  isDrag = false;
+  dragStartPosition = {x: 0, y: 0};
 
   constructor() {
     super();
-    window.onresize = () => this._setViewportSize();
+    window.onresize = () => this.setViewportSize();
   }
 
   firstUpdated() {
-   this._setViewportSize();
+    this.setViewportSize();
   }
 
-  private _setViewportSize() {
+  private setViewportSize() {
     this.viewportWidth = this.svg.clientWidth;
     this.viewportHeight = this.svg.clientHeight;
   }
@@ -141,121 +136,124 @@ export class PanZoomSvg extends LitElement {
   }
 
   get viewboxHeight(): number {
-    return this.zoom * this.viewportHeight;    
+    return this.zoom * this.viewportHeight;
   }
-  
+
   render() {
     return html`
       <svg
         version="1.1"
         width="100%"
         height="100%"
-        viewBox="-50 -500 ${this.viewboxWidth} ${this.viewboxHeight}"
+        viewBox="${-50} ${-this.viewportHeight + 50} ${this.viewboxWidth} ${this
+          .viewboxHeight}"
         xmlns="http://www.w3.org/2000/svg"
-        @click=${(e: MouseEvent) => this.handleSvgClick(e)}
-        @mousemove=${(e: MouseEvent) => this.handleSvgMouseMove(e)}
-        @mouseup=${() => this.handleSvgMouseUp()}
-        @wheel=${(e: WheelEvent) => this.handleSvgWheel(e)}
+        @click=${this.handleSvgClick}
+        @mousemove=${this.handleSvgMouseMove}
+        @mouseup=${this.handleSvgMouseUp}
+        @wheel=${this.handleSvgWheel}
       >
         ${originTemplate({x: 0, y: 0}, 100, 2)}
-        ${this.squares.map((square, id) =>
+        ${this.squares.map(square =>
           squareTemplate(
-            id,
             square,
-            (e) => {e.stopPropagation(); this.handleSquareMouseEnter(square)},
-            (e) => {e.stopPropagation(); this.handleSquareMouseLeave(square)},
-            (e) => {e.stopPropagation(); this.handleSquareClick(square)},
-            (e) => {this._startDrag(e, square)}
+            () => this.handleSquareMouseEnter(square),
+            () => this.handleSquareMouseLeave(square),
+            (e) => this.handleSquareClick(e, square),
+            (e) => this.handleSquareMouseDown(e, square)
           )
         )}
       </svg>
     `;
   }
 
-  private handleSquareMouseEnter(changedSquare: Square) {
-    this.squares = this.squares.map((square) => {
-      if (square === changedSquare) {
-        square.isHighligted = true;
-        return square;
+  private handleSquareMouseEnter(changed: Square) {
+    this.squares = this.squares.map(square => {
+      if (square === changed) {
+        square = {...square, isHighligted: true};
       }
       return square;
     });
   }
 
-  private handleSquareMouseLeave(changedSquare: Square) {
-    this.squares = this.squares.map((square) => {
-      if (square === changedSquare) {
-        square.isHighligted = false;
-        return square;
+  private handleSquareMouseLeave(changed: Square) {
+    this.squares = this.squares.map(square => {
+      if (square === changed) {
+        square = {...square, isHighligted: false};
       }
       return square;
-    })
+    });
   }
 
-  private handleSquareClick(clickedSquare: Square) {
-    this.squares = this.squares.map((square) => {
-      if (square === clickedSquare) {
-        square.isSelected = !square.isSelected;
+  private handleSquareClick(e: MouseEvent, changed: Square) {
+    e.stopPropagation();
+    this.squares = this.squares.map(square => {
+      if (square === changed) {
+        square = {...square, isSelected: !square.isSelected}
       }
       return square;
-    })
+    });
+  }
+
+  private handleSquareMouseDown(e: MouseEvent, changed: Square) {
+    e.stopPropagation();
+    if (changed.isSelected) {
+      this.isDrag = true;
+      this.dragStartPosition = this.clientToSvgUserSpaceCoordinates(e);
+    }
   }
 
   private handleSvgClick(e: MouseEvent) {
     // Print mouse coordinates to console
-    const mousePositon = this.convertClientViewportToSvgUserSpace(e);
-    console.log(`SVG user space [x: ${mousePositon.x}, y: ${mousePositon.y}]`)
+    const mousePositon = this.clientToSvgUserSpaceCoordinates(e);
+    console.log(`SVG user space [x: ${mousePositon.x}, y: ${mousePositon.y}]`);
     // Deselect all squares
     this.squares = this.squares.map((square) => {
-      square.isSelected = false;
-      return square;
-    })
+      return {...square, isSelected: false};
+    });
   }
 
-  private convertClientViewportToSvgUserSpace(e: MouseEvent) {
+  private clientToSvgUserSpaceCoordinates(e: MouseEvent) {
     const CTM = this.svg.getScreenCTM();
     const mousePos = {x: 0, y: 0};
     if (CTM) {
-      mousePos.x = ((e.clientX - CTM.e) / CTM.a);
+      mousePos.x = (e.clientX - CTM.e) / CTM.a;
       mousePos.y = -(e.clientY - CTM.f) / CTM.d;
     }
     return mousePos;
   }
 
-  private _startDrag(evt: MouseEvent, clickedSquare: Square) {
-    if (clickedSquare.isSelected) {
-      this.draggedSqaure = clickedSquare;
-      this.dragStartPointerPosition = this.convertClientViewportToSvgUserSpace(evt);
+  private handleSvgMouseMove(e: MouseEvent) {
+    if (!this.isDrag) {
+      return;
     }
-  }
-
-  private handleSvgMouseMove(evt: MouseEvent) {
-    if (this.draggedSqaure) {
-      const currentPointerPosition = this.convertClientViewportToSvgUserSpace(evt);
-      const deltaX = currentPointerPosition.x - this.dragStartPointerPosition.x;
-      const deltaY = currentPointerPosition.y - this.dragStartPointerPosition.y;
-      this.squares = this.squares.map((square) => {
-        if (square.isSelected) {
-          square.coordinate.x -= deltaX;
-          square.coordinate.y -= deltaY;
-          return square;
-        }
-        return square;
-      })
-      this.dragStartPointerPosition = currentPointerPosition;
-    }
+    const pointerPosition = this.clientToSvgUserSpaceCoordinates(e);
+    const deltaX = pointerPosition.x - this.dragStartPosition.x;
+    const deltaY = pointerPosition.y - this.dragStartPosition.y;
+    this.squares = this.squares.map(square => {
+      if (square.isSelected) {
+        // Move the square to the next position
+        const nextPosition = {
+          x: square.coordinate.x + deltaX,
+          y: square.coordinate.y + deltaY
+        };
+        square = {...square, coordinate: nextPosition};
+      }
+      return square;
+    });
+    this.dragStartPosition = pointerPosition;
   }
 
   private handleSvgMouseUp() {
-    this.draggedSqaure = undefined;
+    this.isDrag = false;
   }
 
-  private handleSvgWheel(evt: WheelEvent) {
-    evt.preventDefault();
+  private handleSvgWheel(e: WheelEvent) {
+    e.preventDefault();
     let zoomFaktor = 1.0;
-    (evt.deltaY < 0) ? zoomFaktor = 0.9 : zoomFaktor = 1.1;
+    e.deltaY < 0 ? (zoomFaktor = 0.9) : (zoomFaktor = 1.1);
     this.zoom /= zoomFaktor;
-  }  
+  }
 }
 
 declare global {
