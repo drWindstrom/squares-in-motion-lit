be509458523118b1b7fb9da1feccf68a1b8445a2
fred <jan.winstroth@outlook.com>
2020-10-14 18:08:33 +0200

    Added path for dragging along a path

--- src/pan-zoom-svg.ts
+++ src/pan-zoom-svg.ts
@@ -1,25 +1,25 @@
-import {LitElement, html, customElement, property, query} from 'lit-element';
-import {Square} from './interfaces/interfaces';
-import {squareTemplate, originTemplate} from './templates';
+import { LitElement, html, customElement, property, query } from 'lit-element';
+import { Square } from './interfaces/interfaces';
+import { squareTemplate, originTemplate } from './templates';
 
 @customElement('pan-zoom-svg')
 export class PanZoomSvg extends LitElement {
-  @property({type: Array})
+  @property({ type: Array })
   squares: Square[] = [];
 
-  @property({type: Number})
+  @property({ type: Number })
   viewBoxMinX = 0;
 
-  @property({type: Number})
+  @property({ type: Number })
   viewBoxMinY = 0;
 
-  @property({type: Number})
+  @property({ type: Number })
   viewportWidth = 0;
 
-  @property({type: Number})
+  @property({ type: Number })
   viewportHeight = 0;
 
-  @property({type: Number})
+  @property({ type: Number })
   zoom = 1;
 
   @query('svg')
@@ -69,7 +69,22 @@ export class PanZoomSvg extends LitElement {
         @mousemove=${this.handleSvgMouseMove}
         @wheel=${this.handleSvgWheel}
       >
-        ${originTemplate({x: 0, y: 0}, 100, 2)}
+        <path
+          id='dragPath'
+          d='M 36.100951,-910.06048 H 827.62683 l 166.68601,166.38064 
+            V -480.6311 L 1208.956,-266.38117 808.8766,-35.81835 
+            H 388.09716 c 0,0 -427.431999,-3.42393 -249.56346,-264.48957
+            205.08356,-301.01038 730.06623,-141.28412 474.9155,-7.436 -255.15068,
+            133.8481 -227.21447,-139.42514 -342.6841,14.87202 -115.46966,
+            154.2971 270.04997,156.15612 270.04997,156.15612 0,0 
+            402.2814,24.16706 430.21759,-107.82208 C 998.96886,-376.527 
+            815.89213,-468.9673 712.15718,-623.77422 672.58182,-682.83374 
+            548.26473,-677.68529 548.26473,-677.68529 H 26.788881'
+          fill='none'
+          stroke='rgb(85,160,185)'
+          stroke-width='2'
+        />
+        ${originTemplate({ x: 0, y: 0 }, 100, 2)}
         ${this.squares.map((square) =>
           squareTemplate(
             square,
@@ -86,7 +101,7 @@ export class PanZoomSvg extends LitElement {
   private handleSquareMouseEnter(changed: Square) {
     this.squares = this.squares.map((square) => {
       if (square === changed) {
-        square = {...square, isHighligted: true};
+        square = { ...square, isHighligted: true };
       }
       return square;
     });
@@ -95,7 +110,7 @@ export class PanZoomSvg extends LitElement {
   private handleSquareMouseLeave(changed: Square) {
     this.squares = this.squares.map((square) => {
       if (square === changed) {
-        square = {...square, isHighligted: false};
+        square = { ...square, isHighligted: false };
       }
       return square;
     });
@@ -112,7 +127,7 @@ export class PanZoomSvg extends LitElement {
     e.stopPropagation();
     this.squares = this.squares.map((square) => {
       if (square === changed) {
-        square = {...square, isSelected: !square.isSelected};
+        square = { ...square, isSelected: !square.isSelected };
       }
       return square;
     });
@@ -123,14 +138,14 @@ export class PanZoomSvg extends LitElement {
     if (e.button === MAIN_BUTTON) {
       // Deselect all squares
       this.squares = this.squares.map((square) => {
-        return {...square, isSelected: false};
+        return { ...square, isSelected: false };
       });
     }
   }
 
   private clientToSvgUserSpaceCoordinates(e: MouseEvent) {
     const CTM = this.svg.getScreenCTM();
-    const mousePos = {x: 0, y: 0};
+    const mousePos = { x: 0, y: 0 };
     if (CTM) {
       mousePos.x = (e.clientX - CTM.e) / CTM.a;
       mousePos.y = -(e.clientY - CTM.f) / CTM.d;
@@ -141,7 +156,7 @@ export class PanZoomSvg extends LitElement {
   isSquareDrag = false;
   isPan = false;
   finishingDrag = false;
-  lastMousePosition = {x: 0, y: 0};
+  lastMousePosition = { x: 0, y: 0 };
   lastUpdate = 0;
 
   private handleSquareMouseDown(e: MouseEvent, changed: Square) {
@@ -180,9 +195,9 @@ export class PanZoomSvg extends LitElement {
           if (square.isSelected) {
             // Move the square to the next position
             square = {
-              ...square, 
+              ...square,
               x: square.x + deltaX,
-              y: square.y + deltaY
+              y: square.y + deltaY,
             };
           }
           return square;
@@ -193,7 +208,7 @@ export class PanZoomSvg extends LitElement {
     }
     // Panning
     if (this.isPan) {
-      const mousePosition = {x: e.clientX, y: e.clientY};
+      const mousePosition = { x: e.clientX, y: e.clientY };
       const deltaX = mousePosition.x - this.lastMousePosition.x;
       const deltaY = mousePosition.y - this.lastMousePosition.y;
       this.viewBoxMinX -= deltaX * this.zoom;
